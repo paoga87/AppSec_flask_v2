989e22ee02bda308635289655f2ec06398f1052b
Paola Garcia <paoga.87@gmail.com>
2019-10-13 21:13:32 -0400

    Add csrf token feature to app

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import subprocess
 import json
 from subprocess import Popen, PIPE, check_output
 from flask import Flask, render_template, redirect, url_for, session, request
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'WxND4o83j4K4iO3762'
 
+#Initialize csrf token
+csrf = CSRFProtect(app)
+
 #Users file
 Users = {}
-#user_file = open("./static/users.txt","r")
-#Users = eval(user_file.read())
-#user_file.close()
 
 
 @app.route('/')
@@ -67,18 +68,20 @@ def login():
 @app.route('/spell_check')
 def spell():
     if(session.get('logged_in') == True): 
-        #cpath = os.getcwd()
-        #if request.method == 'POST':
-            #outputtext = request.form ['inputtext']
-            #textfile = open("./static.text.tx", "w")
-            #textfile.writelines(outputtext)
-            #textfile.close()
+        cpath = os.getcwd()
+        if request.method == 'POST':
+            outputtext = request.form ['inputtext']
+            textfile = open("./static.text.tx", "w")
+            textfile.writelines(outputtext)
+            textfile.close()
 
-            #tmp = subprocess.check_output([cpath + '/static/a.out',cpath + '/static/text.text', cpath + '/static/wordlist.txt']).decode('utf-8')
-            #mispelled = tmp.replace("\n",", ")[:-2]
-            #return render_template("spell_check.html", mispelled = mispelled, outputtext = outputtext)
+            tmp = subprocess.check_output([cpath + '/static/a.out',cpath + '/static/text.text', cpath + '/static/wordlist.txt']).decode('utf-8')
+            mispelled = tmp.replace("\n",", ")[:-2]
+            return render_template("spell_check.html", mispelled = mispelled, outputtext = outputtext)
 
-        return render_template("spell_check.html")
+        if request.method == 'GET':
+            return render_template("spell_check.html")
+
     else:
         return redirect(url_for('login'))
     
